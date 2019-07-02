d0820f2048816e91995e1ad8a2c5836c69a4aaf5
DimYog <dimasyogapra@gmail.com>
2019-07-02 11:08:25 +0700

    use qt designer

---
+++ display.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'display.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.price_box = QtWidgets.QTextEdit(self.centralwidget)
+        self.price_box.setGeometry(QtCore.QRect(130, 79, 191, 41))
+        self.price_box.setObjectName("price_box")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 80, 81, 31))
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.tax_rate = QtWidgets.QSpinBox(self.centralwidget)
+        self.tax_rate.setGeometry(QtCore.QRect(140, 160, 81, 28))
+        self.tax_rate.setProperty("value", 20)
+        self.tax_rate.setObjectName("tax_rate")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(40, 170, 74, 18))
+        self.label_2.setObjectName("label_2")
+        self.calc_tax_button = QtWidgets.QPushButton(self.centralwidget)
+        self.calc_tax_button.setGeometry(QtCore.QRect(140, 210, 111, 34))
+        self.calc_tax_button.setObjectName("calc_tax_button")
+        self.results_window = QtWidgets.QTextEdit(self.centralwidget)
+        self.results_window.setGeometry(QtCore.QRect(140, 260, 104, 70))
+        self.results_window.setObjectName("results_window")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Price"))
+        self.label_2.setText(_translate("MainWindow", "Tax Rate"))
+        self.calc_tax_button.setText(_translate("MainWindow", "calculate tax"))
